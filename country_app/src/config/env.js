600b46af45909ade2fb9a7b12f55e382c28ef4db
Vihanga Upamal <upamal2001@gmail.com>
2025-05-16 10:22:57 +0530

    Update env.js

--- country_app/src/config/env.js
+++ country_app/src/config/env.js
@@ -4,10 +4,10 @@ const env = {
         API_BASE_URL: 'http://13.53.32.227:8091'
     },
     test: {
-        API_BASE_URL: 'http://192.168.1.4:8091'
+        API_BASE_URL: 'http://13.53.32.227:8091'
     },
     production: {
-        API_BASE_URL: 'https://your-render-app-name.onrender.com' // Replace with your Render.com URL
+        API_BASE_URL: 'http://13.53.32.227:8091' // Replace with your Render.com URL
     }
 };
 
