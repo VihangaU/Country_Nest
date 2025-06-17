287454c77fa71687b7bfc1a4b93dd1118ca87208
Vihanga Upamal <upamal2001@gmail.com>
2025-06-17 18:21:26 +0530

    Update env.js

--- country_app/src/config/env.js
+++ country_app/src/config/env.js
@@ -1,13 +1,13 @@
 const env = {
     development: {
         // API_BASE_URL: 'http://192.168.1.4:8091'
-        API_BASE_URL: 'https://countrynestback.dynac.space'
+        API_BASE_URL: 'http://localhost:8091'
     },
     test: {
-        API_BASE_URL: 'https://countrynestback.dynac.space'
+        API_BASE_URL: 'http://localhost:8091'
     },
     production: {
-        API_BASE_URL: 'https://countrynestback.dynac.space' // Replace with your Render.com URL
+        API_BASE_URL: 'http://localhost:8091' // Replace with your Render.com URL
     }
 };
 
